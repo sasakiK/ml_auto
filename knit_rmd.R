77de61cd9a9c6c60cc42cfd55e0610a8034914e8
sasakiK <sskcoobbbn4@yahoo.co.jp>
2019-10-26 04:02:55 +0900

    [update] files

---
+++ knit_rmd.R
@@ -0,0 +1,15 @@
+# Description -------------------------------------------------------------
+
+#' @ Description:Rmarkdownを実行するためのスクリプト
+#' @ Auther     :sasaki_K
+
+# knit rmd ----------------------------------------------------------------
+
+group_name <- commandArgs(trailingOnly = TRUE)[1]
+current_time <- commandArgs(trailingOnly = TRUE)[2]
+
+output_path <- paste("output/experiment_",current_time,"/",sep="")
+
+rmarkdown::render(input = "modeling.Rmd",
+                  output_dir = output_path,
+                  output_file = paste("modeling_", group_name, ".html", sep = ""))
